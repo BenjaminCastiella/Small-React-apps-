aee432beacd58802bec85cc73f7cc95be151a04f
BenjaminCastiella <benjamincastiella78@gmail.com>
2025-01-13 15:20:53 -0300

    An improvement

--- an-app/src/like-button.jsx
+++ an-app/src/like-button.jsx
@@ -1,7 +1,21 @@
 import React, { useState } from 'react'
 
 export function LikeButton(){
+    const [isLiked, setIsLiked] = useState(false)
     const [likes, setLike] = useState(0)
+    const [text, setText] = useState("Like")
 
-    return <button className='like-btn' onClick={() => setLike(likes + 1)}>Likes <span>{likes}</span></button>
+    const handleClick = ()=>{
+        if(!isLiked){
+            setLike(likes + 1);
+            setText("Dislike");
+            setIsLiked(true);
+        } else{
+            setLike(likes - 1);
+            setText("Like");
+            setIsLiked(false);
+        }
+    } 
+
+    return <button className='like-btn' onClick={() => handleClick()}>{text} <span>{likes}</span></button>
 }
